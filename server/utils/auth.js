2230e61af85fc7554167ad61f19a3ab5da88a6a9
Wayne <waynefenwick@gmail.com>
2023-08-13 08:46:09 -0500

    Made first change to server/utils/app.js file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -26,14 +26,3 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
-
-// Changes made to the updated auth.js:
-
-// Instead of destructuring {req} from the parameter, use the context object directly.
-// Remove the check for req.body.token and req.query.token since these are typically not used in GraphQL. The token is usually passed through the Authorization header.
-// Modify how the token is extracted from the Authorization header.
-// Instead of modifying the req object directly, set context.user to the decoded user data.
-// Please note that the exact implementation might depend on how your GraphQL server is structured and how authentication is handled in your GraphQL resolvers.
-// Make sure to integrate this updated auth.js according to your specific GraphQL setup.
-
-// TESTED NPM START ON CLIENT AND SERVER: BOTH LOADED SUCESSFULLY WITH CHANGES
